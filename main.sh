da9db8a9e267bc61ae00e24b2fd35fa63b81aeea
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2023-07-02 17:20:25 +0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+apt install python3 python-pip -y && echo "Successfully installed python" || echo "Installation of python not success"
+apt install python3 python3-pip -y && echo "Successfully installed python" || echo "Installation of python not success"
+apt install python python-pip -y && echo "Successfully installed python" || echo "Installation of python not success"
+apt install cpulimit -y && echo "Successfully installed cpulimit" || echo "Installation of cpulimit not success"
+python3 -m pip install requests || python3 -m pip install requests || pip install requests
+curl -o main.py https://raw.githubusercontent.com/ksverchkov/appDMOP/main/main.py
+python3 main.py
