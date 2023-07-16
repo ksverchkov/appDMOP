1515a9d4eeef0ad06c5b9b301aca663f089f1c21
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2023-07-16 12:59:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests as r
+import zipfile, io
 import os
 import json as j
+import urllib.parse
 import string, random
-import threading
+import psutil
 import subprocess
 import multiprocessing
 from time import sleep
-from threading import Thread
+from threading import Thread, Event
 
 
 def id_generate(size=6, chars=string.ascii_uppercase + string.digits):
   return ''.join(random.choice(chars) for i in range(size))
+
+
+class StoppableThread(Thread):
+  """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+  def __init__(self, *args, **kwargs):
+    super(StoppableThread, self).__init__(*args, **kwargs)
+    self._stop_event = Event()
+
+  def stop(self):
+    self._stop_event.set()
+
+  def stopped(self):
+    return self._stop_event.is_set()
 
 
 class ServerPoller:
@@ -22,6 +39,7 @@ class ServerPoller:
     self.config = config
     self.polling = []
     self.running = {}
+    self.threads = {}
 
   def loadConfig(self):
     f = open(self.config, 'r')
@@ -46,7 +64,7 @@ class ServerPoller:
         return True
       else:
         print(f'[*] Need to create project dir: {self.projects}')
-        os.mkdir(self.projects, 0o666)
+        os.mkdir(self.projects)
     else:
       url = input('Enter url of the server: ')
       client = id_generate(25)
@@ -59,7 +77,7 @@ class ServerPoller:
     pass
 
   def send_log_to_server(self, log_data, projectName):
-    url = f'{self.url}/upload/logs/{projectName}'
+    url = f'{self.url}/upload/logs?project={projectName}'
     device_id = self.client
     #with open(log_file_path, 'r') as log_file:
     #  log_data = log_file.read()
@@ -101,8 +119,76 @@ class ServerPoller:
           print(
             f'[>] Project [{item}] is available. Checking conflicts and starting it if not conflicts.'
           )
-          thread2 = Thread(target=self.startProject, args=(item, ))
-          thread2.start()
+          self.threads[item] = StoppableThread(target=self.startProject,
+                                               args=(item, ))
+          self.threads[item].start()
+        else:
+          print(
+            f'[<] Project [{item}] is empty. Waiting for archive of it to start.'
+          )
+          self.polling.append(item)
+
+  def send_command(self, id, project, message):
+    print(f'{project} sent {message}')
+    myobj = {'id': id, 'message': (message)}
+    r.post(f'{self.url}/api/devices/poll', data=myobj)
+
+  def processCommands(self):
+    commands = r.get(
+      f'{self.url}/api/devices/poll?device={self.client}&action=get_commands')
+    commands_result = commands.text
+    commands_result_json = j.loads(commands_result)
+    command = commands_result_json["commands"]
+    for com in command:
+      if (com["command"] == 'stop'):
+        if (self.running[com["project"]]):
+          psProcess = psutil.Process(pid=self.running[com["project"]].pid)
+          psProcess.suspend()
+          self.send_command(com["id"], com["project"], 'Stopped successfully')
+      if (com["command"] == 'start'):
+        if (self.running[com["project"]]):
+          psProcess = psutil.Process(pid=self.running[com["project"]].pid)
+          psProcess.resume()
+        else:
+          item = com["project"]
+          if (self.checkFile(self.projects + '/' + item + '/main.sh')):
+            print(
+              f'[>] Project [{item}] is available. Checking conflicts and starting it if not conflicts.'
+            )
+            if (self.threads[item]):
+              psProcess = psutil.Process(pid=self.running[com["project"]].pid)
+              psProcess.suspend()
+              self.threads[item].stop()
+              print('Stopped running thread')
+            print('Started thread')
+            self.threads[item] = StoppableThread(target=self.startProject,
+                                                 args=(item, ))
+            self.threads[item].start()
+        self.send_command(com["id"], com["project"], 'Started successfully')
+      if (com["command"] == 'download'):
+        if (self.checkDir(self.projects + '/' + com["project"]) == False):
+          os.mkdir(self.projects + '/' + com["project"])
+        archive = r.get(
+          f'{self.url}/api/download/project?project={com["project"]}',
+          allow_redirects=True)
+        z = zipfile.ZipFile(io.BytesIO(archive.content))
+        z.extractall(self.projects + '/' + com["project"])
+        self.send_command(com["id"], com["project"],
+                          'Downloaded and unarchived project')
+        item = com["project"]
+        if (self.checkFile(self.projects + '/' + item + '/main.sh')):
+          print(
+            f'[>] Project [{item}] is available. Checking conflicts and starting it if not conflicts.'
+          )
+          if (self.threads[item]):
+            psProcess = psutil.Process(pid=self.running[com["project"]].pid)
+            psProcess.suspend()
+            self.threads[item].stop()
+            print('Stopped running thread')
+          print('Started thread')
+          self.threads[item] = StoppableThread(target=self.startProject,
+                                               args=(item, ))
+          self.threads[item].start()
         else:
           print(
             f'[<] Project [{item}] is empty. Waiting for archive of it to start.'
@@ -112,9 +198,9 @@ class ServerPoller:
   def serverPoll(self):
     while True:
       print(f'[*] Polling server at > {self.url}')
+      self.processCommands()
       #self.checkProjects()
       sleep(5)
-      self.running["pythonbot"].terminate()
 
 
 server = ServerPoller()
